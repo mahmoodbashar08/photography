88176479520064ab0a341b2670b04211daaa79c1
mahmood <zozo99999@icloud.com>
2022-09-27 03:47:42 +0300

    .

--- Client/app.py
+++ Client/app.py
@@ -1,22 +1,3 @@
-from flask_mysqldb import MySQL
-import sys
-sys.path.append('..')
-from flask import Flask, render_template
+from flask import Flask, template_rendered, request
+import sqlite3
 
-app = Flask(__name__, template_folder="templates")
-mysql = MySQL(app)
-
-@app.route("/<name>")
-def index(name):
-    return render_template('/index.html', var=name)
-
-
-@app.route("/photo/1")
-def display():
-    cur = mysql.connection('photography.db')
-    cur.execute('SELECT * FROM photos')
-    rv = cur.fetchall()
-    return str(rv)
-
-if __name__ == "__main":
-    app.run()
